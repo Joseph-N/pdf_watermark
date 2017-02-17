ca884e252a5d713c94a2fd716f5f940646dd5c2c
Bruce Shi <shibocuhk@gmail.com>
2017-02-17 15:33:03 +0800

    remove p

--- spec/pdf_watermark/pdf_watermark_spec.rb
+++ spec/pdf_watermark/pdf_watermark_spec.rb
@@ -5,7 +5,6 @@ describe PdfWatermark do
   it 'create watermark' do
     source = File.join(test_root, 'data', 'test.pdf')
     dest = File.join(test_root, 'data', 'result.pdf')
-    p source
     PdfWatermark.watermark('Private And Confidential ** For 黄依群 of 基石资本 Only ** Do Not Distribute',
                            source, dest, options: { :angle => :diagonal, :font_color => "999999", :transparent => 0.5 })
 
