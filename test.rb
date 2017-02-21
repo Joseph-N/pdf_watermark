352f375dd0677f2a0e535d3a611d5f1e622a5346
Bruce Shi <shibocuhk@gmail.com>
2017-02-21 15:37:25 +0800

    remove test.rb

--- test.rb
+++
@@ -1,8 +0,0 @@
-require 'pdf_watermark'
-require 'ruby-prof'
-RubyProf.start
-PdfWatermark.watermark('Private And Confidential ** For 黄依群 of 基石资本 Only ** Do Not Distribute',
-                       "test.pdf", "result.pdf", options: { :angle => :diagonal, :font_color => "999999", :transparent => 0.5 })
-result = RubyProf.stop
-printer = RubyProf::FlatPrinter.new(result)
-printer.print(STDOUT)
